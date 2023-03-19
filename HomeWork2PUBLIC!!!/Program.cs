e60ea5647539b6ad6e6e6e984e07e96bbedd1f88
DmitryPechenkin <dimape4enb1998@gmail.com>
2023-03-19 19:51:09 +0300

    Добавьте файлы проекта.

---
+++ HomeWork2PUBLIC!!!/Program.cs
@@ -0,0 +1,19 @@
+﻿
+try
+{
+    var array = new int[] { 4, 6, 10, 35, 178, 43, 77, 51 };
+    var max = 10000;
+    var secMax = 0;
+    foreach (var item in array)
+    {
+        if (max < item)
+        {
+            max = item;
+        }
+    }
+    Console.WriteLine(secMax);
+}
+finally
+{
+    Console.WriteLine("Ошибка. Пожалуйста, перепроверьте введёные данные");
+}
